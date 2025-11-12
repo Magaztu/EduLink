b76332465440d30c784f79abbaff22396a4d414d
Xavier Carpio <cxavier@est.ecotec.edu.ec>
2025-11-11 19:26:19 -0500

    Evento y Interfaz nuevos

---
+++ EduLink.Application/Interfaces/IProveedorRepository.cs
@@ -0,0 +1,9 @@
+using EduLink.Domain.Entities;
+
+namespace EduLink.Application.Interfaces;
+
+public interface IProveedorRepository
+{
+    Task<Proveedor> ObtenerPorIdAsync(Guid id);
+    Task GuardarAsync(Proveedor proveedor);
+}
